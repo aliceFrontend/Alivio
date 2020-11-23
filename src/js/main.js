ea9bd79345085b2b2b81c7b3c5c9d31c0e0f1753
aliceFrontend <aliceFrontend@users.noreply.github.com>
2020-11-23 15:11:22 +0300

    Create modal map
    Create new modal "Map"

--- src/js/main.js
+++ src/js/main.js
@@ -1,96 +1,88 @@
-// $(document).ready(function(){
-//   var link = $('.menu-link');
-//   var link__active = $('.menu-link__active');
-//   var menu = $('.header__navs');
-//   var nav__link = $('.header__navs a');
-
-//   link.click(function(){
-//     link.toggleClass('menu-link__active');
-//     menu.toggleClass('header__navs-active');
-//   })
-//   nav__link.click(function(){
-//     link.toggleClass('menu-link__active');
-//     menu.toggleClass('header__navs-active');
-//   })
-// })
-
 window.onload = function(){
 
-  let menu__link = document.querySelector('.menu-link');
-  let header__navs = document.querySelector('.header__navs');
-  let hidden = document.querySelector('body');
-
-  let auth__sign_btn = document.querySelector('.auth__sign-btn');
-  let auth__sign_btn_mobile = document.querySelector('.auth__sign-btn-mobile');
-  let modal__wrapper = document.querySelector('.modal__wrapper');
-  let cross = document.querySelector('.modal__close');
-  let modal__show = document.querySelector('.modal');
+const menuLink = document.querySelector('.menu-link'),
+      headerNavs = document.querySelector('.header__navs'),
+      hidden = document.querySelector('body'),
+      authSignBtn = document.querySelector('.auth__sign-btn'),
+      authSignBtnMobile = document.querySelector('.auth__sign-btn-mobile'),
+      modalWrapper = document.querySelector('.modal__wrapper'),
+      cross = document.querySelector('.modal__close'),
+      modalShow = document.querySelector('.modal');
 
   function CreateAndDelete(){
 
-    menu__link.classList.toggle('menu-link__active');
-    header__navs.classList.toggle('header__navs-active');
+    menuLink.classList.toggle('menu-link__active');
+    headerNavs.classList.toggle('header__navs-active');
     hidden.classList.toggle('hidden');
   }
+  function ModalShow(){
 
-  menu__link.addEventListener("click", CreateAndDelete);
-  // header__navs.addEventListener("click", CreateAndDelete);
-  
-  auth__sign_btn.addEventListener("click",()=>{
-    modal__wrapper.classList.toggle('modal__active');
-    modal__show.classList.toggle('modal__show');
+    modalWrapper.classList.toggle('modal__active');
+    modalShow.classList.toggle('modal__show');
     hidden.classList.toggle('hidden');
-    let body = document.querySelector('body');
-     hidden.addEventListener("click", ()=>{
-        console.log("click!");
-      });
-  });
+    const body = document.querySelector('body');
+     // hidden.addEventListener("click", ()=>{
+     //    console.log("click!");
+     //  });
+  }
+  function ModalActive(){
 
-  auth__sign_btn_mobile.addEventListener("click",()=>{
-    modal__wrapper.classList.toggle('modal__active');
-    modal__show.classList.toggle('modal__show');
+    modalWrapper.classList.toggle('modal__active');
+    modalShow.classList.toggle('modal__show');
     hidden.classList.toggle('hidden');
-  });
+  }
+  function Close(){
+    modalWrapper.classList.toggle('modal__active');
+    modalShow.classList.toggle('modal__show');
+    hidden.classList.toggle('hidden');
+  }
+  function TouctStartMobile(event){
 
-  cross.addEventListener("click",()=>{
-  modal__wrapper.classList.toggle('modal__active');
-  modal__show.classList.toggle('modal__show');
-  hidden.classList.toggle('hidden');
-  });
-
-  let lastMove = null;
-  let start = 0;
-  let step = 0; 
-  let result = 0;
-  let header__navs_act = "";
-
-   hidden.addEventListener("touchstart", (event)=>{
-    
-      header__navs_act = document.querySelector('.header__navs-active');
+      headerNavsAct = document.querySelector('.header__navs-active');
       lastMove = event;
       start = lastMove.changedTouches[0].pageX;
-  });
+  }
+  function TouchMoveMobile(event){
+    lastMove = event;
+    step = lastMove.changedTouches[0].pageX;
+    headerNavsAct.style.transition = "right 1s ease-out 0.5s";
+    headerNavsAct.style.transform = "translateX("+-(start-step)+"px)";
+  }
+  function TouchEndMibile(event){
 
-    hidden.addEventListener("touchmove", (event)=>{
-      
-      lastMove = event;
-      step = lastMove.changedTouches[0].pageX;
-      header__navs_act.style.transition = "right 1s ease-out 0.5s";
-      header__navs_act.style.transform = "translateX("+-(start-step)+"px)";
-  });
-
-    hidden.addEventListener("touchend", (event)=>{
-    
-      result = start - step;
+     result = start - step;
       if(-(result) < -400){
 
         CreateAndDelete();
-        header__navs_act.style.transform = "none";
+        headerNavsAct.style.transform = "none";
       } else{ 
-        header__navs_act.style.transform = "none";
+        headerNavsAct.style.transform = "none";
       }
-      // console.log("result" + result);
 
-    });
+  }
+
+  menuLink.addEventListener("click", CreateAndDelete);
+  authSignBtn.addEventListener("click", ModalShow);
+  authSignBtnMobile.addEventListener("click", ModalActive);
+  cross.addEventListener("click", Close);
+
+  let lastMove = null,
+      start = 0,
+      step = 0,
+      result = 0,
+      headerNavsAct = "";
+
+   hidden.addEventListener("touchstart", TouctStartMobile);
+   hidden.addEventListener("touchmove", TouchMoveMobile);
+   hidden.addEventListener("touchend", TouchEndMibile);
  }
 
+var map = L.map('mapid').setView([54.762,37.375], 8); // Карта внутри блока #map
+
+L.tileLayer('http://vec{s}.maps.yandex.net/tiles?l=map&v=4.55.2&z={z}&x={x}&y={y}&scale=2&lang=ru_RU', {
+    subdomains: ['01', '02', '03', '04'],
+    attribution: '<a http="yandex.ru" target="_blank">Яндекс</a>',
+    reuseTiles: true,
+    updateWhenIdle: false
+  }
+).addTo(map);
